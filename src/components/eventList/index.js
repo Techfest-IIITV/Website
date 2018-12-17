a17b74c38789609fea8c6765220000704ddce5b0
Jay Mistry <mistry.jay15@gmail.com>
2018-12-18 00:13:33 +0530

    Card size tweak

--- src/components/eventList/index.js
+++ src/components/eventList/index.js
@@ -5,31 +5,30 @@ import Panel from "../Panel";
 import './styles.css';
 
 class EventList extends React.Component {
-
-  render() {
-
+  render () {
     let listEventsJsx = []
 
-    if ( this.props.event ) {
-      let events = this.props.event.events;
-      let listEvents = [];
+    if (this.props.event) {
+      console.log('Helloooo')
+      let events = this.props.event.events
+      let listEvents = []
       while (events.length !== 0) {
         let arr = [...events.splice(0, 3)]
         arr = arr.filter(a => a !== undefined)
         listEvents.push(arr)
       }
-      
-      const listEventsLength = listEvents.length
+
+      // const listEventsLength = listEvents.length
 
       listEventsJsx = listEvents.map((chunk, i) => {
         return  <div key={i} className=' event-list section'>
-          <div className={'uk-text-center uk-margin-medium-bottom'}>
+          {/* <div className={'uk-text-center uk-margin-medium-bottom'}>
             <span className={'event-list-heading'}>PAST</span>
             {' '}
             <span className={'event-list-heading'}>EVENTS</span>
             {' '}
             <span className={'event-list-heading'}>{`${i+1}/${listEventsLength}`}</span>
-          </div>
+          </div> */}
           <div uk-grid="true" className={'uk-grid-large uk-child-width-expand@s'}>
             {chunk.map(c => {
               return (<Panel key={c.id} event={c}/>)
@@ -38,10 +37,9 @@ class EventList extends React.Component {
         </div>
       })
     }
-    
     return (
       <Fragment>
-        {listEventsJsx ? listEventsJsx : (<div style={{color: 'white'}} className='section'>Loading...</div>)}
+        {listEventsJsx}
       </Fragment>
     );
   }
